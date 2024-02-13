62ea92608117e9bfaede0abb366b81b2c52cd105
MS120s <umarbalogunsulaiman@gmail.com>
2024-02-13 17:48:35 +0100

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+const switchBtn = document.getElementById("switch-btn")
+
+switchBtn.addEventListener("click", function () {
+    switchBtn.classList.toggle('on')
+})
+
+function change() {
+    
+}
